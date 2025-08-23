06dd6c07425b2674837079608cf49266941869d5
Ojas <ojas1807@gmail.com>
2025-08-23 15:45:53 -0400

    Application page

---
+++ app/application/page.tsx
@@ -0,0 +1,25 @@
+
+export default function ContactPage() {
+    return (
+        <div className="min-h-screen bg-background overflow-hidden">    
+        <iframe
+        src="https://tally.so/r/wM41vY"
+        width="100%"
+        height="100vh"
+        style={{
+          position: "fixed",
+          top: 75,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          border: 0,
+          margin: 0,
+          padding: 0,
+        }}
+        allowFullScreen
+      />
+
+        </div>
+
+    );
+  }
